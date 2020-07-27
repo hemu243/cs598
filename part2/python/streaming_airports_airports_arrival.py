2bab4699ae7ecf102b8a6e0aa9aed695d3b2688b
hemu243 <hchoudhary@splunk.com>
2020-07-27 00:06:58 -0700

    add spark session

--- part2/python/streaming_airports_airports_arrival.py
+++ part2/python/streaming_airports_airports_arrival.py
@@ -6,6 +6,7 @@ from pyspark.sql import SparkSession
 from kafka import KafkaProducer
 import sys
 
+
 def printResults(rdd):
     """
     Print partial results to screen.
@@ -15,16 +16,18 @@ def printResults(rdd):
         print(line)
     print("SIZE: %d" % rdd.count())
 
+
 def saveResults(rdd):
-	"""
+    """
 	Save results as a report.
 	"""
-	results = rdd.collect();
-	if len(results) == 0:
-		return
-	file = open(sys.argv[2], 'w')
-	for item in results:
-		file.write("\n%s -> %s: %s\n\n" % (item[0][0], item[0][1], item[1]))
+    results = rdd.collect();
+    if len(results) == 0:
+        return
+    file = open(sys.argv[2], 'w')
+    for item in results:
+        file.write("\n%s -> %s: %s\n\n" % (item[0][0], item[0][1], item[1]))
+
 
 def updateFunction(newValues, runningAvg):
     if runningAvg is None:
@@ -35,20 +38,23 @@ def updateFunction(newValues, runningAvg):
     avg = prod / float(count)
     return (prod, count, avg)
 
+
 def sendToKafka(records):
-	"""
+    """
 	Send records to Kafka. The format is the following
 	JFK LAX -3.013333
 	JFK ORD 0.01113
 	"""
-	producer = KafkaProducer(bootstrap_servers='b-2.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092,b-1.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092')
-	for record in records:
-		message = "%s %s %s" % (record[0][0], record[0][1], record[1])
-		producer.send_messages('airports_airports_arrival', message.encode())
+    producer = KafkaProducer(
+        bootstrap_servers='b-2.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092,b-1.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092')
+    for record in records:
+        message = "%s %s %s" % (record[0][0], record[0][1], record[1])
+        producer.send_messages('airports_airports_arrival', message.encode())
+
 
 # MAIN
 
-sc = SparkSession.builder.appName(appName="AirportAirportArrival").getOrCreate()
+sc = SparkSession.builder.appName("AirportAirportArrival").getOrCreate()
 # sc = SparkContext(appName="AirportAirportArrival")
 # sc.setLevel('ERROR')
 
@@ -56,8 +62,9 @@ sc = SparkSession.builder.appName(appName="AirportAirportArrival").getOrCreate()
 # ssc = StreamingContext(sc, 1)
 # ssc.checkpoint("s3a://hsc4-cc-part2-streaming/checkpoints/checkpoint-airport-airport-arrival")
 
-lines = sc.readStream().format("kafka").option("kafka.bootstrap.servers", "b-2.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092,b-1.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092")\
-	.option("subscribe", "input").load()
+lines = sc.readStream().format("kafka").option("kafka.bootstrap.servers",
+                                               "b-2.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092,b-1.kafka-cluster-1.rp7oyu.c8.kafka.us-east-1.amazonaws.com:9092") \
+    .option("subscribe", "input").load()
 
 # lines = KafkaUtils.createDirectStream(ssc, ['input'], {"metadata.broker.list": sys.argv[1], "auto.offset.reset":"smallest"})
 
@@ -73,8 +80,8 @@ airports_fromto = rows.map(lambda row: ((row[0], row[1]), float(row[8])))
 airports_fromto = airports_fromto.updateStateByKey(updateFunction)
 # Change key to just airports
 airports = airports_fromto.map(lambda row: ((row[0][0], row[0][1]), row[1][2]))
-#Filter and print
-airports = airports.filter(lambda x: x[0] in [('LGA','BOS'),('BOS','LGA'),('OKC','DFW'),('MSP','ATL')])
+# Filter and print
+airports = airports.filter(lambda x: x[0] in [('LGA', 'BOS'), ('BOS', 'LGA'), ('OKC', 'DFW'), ('MSP', 'ATL')])
 
 airports.foreachRDD(printResults)
 airports.foreachRDD(saveResults)
@@ -82,5 +89,5 @@ airports.foreachRDD(saveResults)
 # Kafka sink
 airports.foreachRDD(lambda rdd: rdd.foreachPartition(sendToKafka))
 
-ssc.start()             # Start the computation
+ssc.start()  # Start the computation
 ssc.awaitTermination()  # Wait for the computation to terminate
